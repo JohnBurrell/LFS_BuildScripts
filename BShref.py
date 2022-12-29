ffd1f755b31ba6eb1320bbe5a26639a523e3b70e
John Burrell <John_burrell@hotmail.com>
2022-12-29 11:42:00 +0000

    Add files via upload

--- BShref.py
+++ BShref.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
 import urllib.request, urllib.parse, urllib.error
+import lxml
 import sys, os
 url=sys.argv[1]
 exts = (".xz", ".bz2", ".gz", ".lzma", ".tgz", ".zip", ".patch", ".deb", ".jar")
 packages = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(packages, "lxml")
+soup = BeautifulSoup(packages, features="xml")
 for link in soup.find_all('a'):
     if link.get('href'): # remove None
         for anhref in link.get('href').split():
